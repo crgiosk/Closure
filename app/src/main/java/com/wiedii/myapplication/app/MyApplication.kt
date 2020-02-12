d3c754a6cc2470f7740ec2bd45e356b127e341d1
Sergio Noriega <sergio.noriega@wiedii.co>
2020-02-12 17:09:47 -0500

    crea db, proseguir con guardado y consulta desde la bd

--- app/src/main/java/com/wiedii/myapplication/app/MyApplication.kt
+++ app/src/main/java/com/wiedii/myapplication/app/MyApplication.kt
@@ -1,4 +1,18 @@
 package com.wiedii.myapplication.app
 
-class MyApplication {
+import android.app.Application
+import com.wiedii.myapplication.modules.applicationModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class MyApplication : Application(){
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@MyApplication)
+            androidLogger()
+            modules(listOf(applicationModule))
+        }
+    }
 }
--- app/src/main/java/com/wiedii/myapplication/view/fragments/NuevoHeroeFragment.kt
+++ app/src/main/java/com/wiedii/myapplication/view/fragments/NuevoHeroeFragment.kt
@@ -6,14 +6,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
 
 import com.wiedii.myapplication.R
+import com.wiedii.myapplication.classes.Heroes
+import com.wiedii.myapplication.viewmodels.HeroeViewModel
+import com.wiedii.myapplication.viewmodels.UiState
+import kotlinx.android.synthetic.main.fragment_heroes.*
+import kotlinx.android.synthetic.main.fragment_nuevo_heroe.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-/**
- * A simple [Fragment] subclass.
- */
 class NuevoHeroeFragment : Fragment() {
 
+    private val heroeViewModel: HeroeViewModel by viewModel()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -21,6 +27,31 @@ class NuevoHeroeFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_nuevo_heroe, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupListener()
+    }
+
+    fun setupListener(){
+        nuevoHeroeFabButton.setOnClickListener {
+            heroeViewModel.saveHeroe(
+                Heroes(
+                    nombre = nombreHeroeEditext.text.toString(),
+                    tipo = tipoHeroeEditext.text.toString(),
+                    categoria = categorieHeroreEditext.text.toString(),
+                    descripcion = descriptionEditext.text.toString(),
+                    imageUrl = urlImagenEditext.text.toString()
+                )
+            )
+        }
+    }
+
+    fun setHandlers(){
+
+    }
+
+
 
 
 }
